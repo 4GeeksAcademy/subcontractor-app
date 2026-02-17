dd44ff5e13172ace9d70281f8a3e6c5b33c306f3
cinjude <110071132+cinjude@users.noreply.github.com>
2026-02-17 20:18:04 +0000

    [db]

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,62 @@
+import enum
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, Enum, DateTime, func, ForeignKey, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
+class UserRole(enum.Enum):
+    stylist = "stylist"
+    client = "client"
+
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(120))
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False)
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    stylist: Mapped[list['Stylist']] = relationship(back_populates='user')
+    client: Mapped[list['Client']] = relationship(back_populates='userClient')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
+class Stylist(db.Model):
+    __tablename__ = 'stylist'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    bio: Mapped[str] = mapped_column(String(500))
+    phone: Mapped[int] = mapped_column(Integer, nullable=False)
+    address: Mapped[float] = mapped_column(String(120), nullable=False)
+    latitude: Mapped[float] = mapped_column(Float, nullable=False)
+    longitude: Mapped[float] = mapped_column(Float, nullable=False)
+    profile_image: Mapped[str] = mapped_column(String, nullable=False)
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
+
+    user: Mapped['User'] = relationship(back_populates='stylist')
+
+
+class Client(db.Model):
+    __tablename__ = 'client'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    address: Mapped[str] = mapped_column(String(120), nullable=True)
+    phone: Mapped[int] = mapped_column(Integer, nullable=True)
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
+
+    userClient: Mapped['User'] = relationship(back_populates='client')
